9a619260110511c736658a9d462d0e8b341f19b8
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-11-01 12:46:45 +0000

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zooniverse React Transcriber
+
+A generic React-based Transcription project powered by the Zooniverse.
+
+## Development
+
+* This is built on top of the [Zooniverse Reduxify](https://github.com/zooniverse/zoo-reduxify) project.
+
+Starting the project:
+
+1. `npm install` to install dependencies.
+2. `npm start` to start the project.
+3. `http://localhost:4000` to view the project on your web browser.
---
+++ src/actions/login.js
@@ -0,0 +1,39 @@
+import { browserHistory } from 'react-router';
+import apiClient from 'panoptes-client'
+import oauth from 'panoptes-client/lib/oauth'
+import * as types from '../constants/actionTypes';
+import config from '../constants/config';
+
+
+export function checkLoginUser() {  //First thing on app load - check if the user is logged in.
+  return (dispatch) => {
+    oauth.checkCurrent()
+      .then((user) => {
+        dispatch(setLoginUser(user));
+      });
+  }
+}
+
+export function setLoginUser(user) {
+  return (dispatch) => {
+    dispatch({
+      type: types.SET_LOGIN_USER,
+      user
+    });
+  };
+}
+
+export function loginToPanoptes() {  //Returns a login page URL for the user to navigate to.
+  return (dispatch) => {
+    return oauth.signIn(window.location.href.split('#')[0])
+  }
+}
+
+export function logoutFromPanoptes() {
+  return (dispatch) => {
+    oauth.signOut()
+      .then(user => {
+        dispatch(setLoginUser(user));
+      });
+  }
+}
