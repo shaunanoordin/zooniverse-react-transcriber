0df452dbfc2da82e2f04bbb5528b10cbed2a179f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-11-03 16:34:20 +0000

    Added simple start to the Transcribe pages

---
+++ src/components/Transcribe/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Transcribe...</h2>
+        <svg width="100" height="100">
+          <circle cx="50" cy="50" r="40" stroke="#999" strokeWidth="4" fill="#c33" />
+        </svg>
+      </div>
+    );
+  }
+}
