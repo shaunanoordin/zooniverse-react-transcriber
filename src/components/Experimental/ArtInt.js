7fb2a16baa639e4390f526ae7a39ef375305b4f5
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-02-21 15:01:47 +0000

    Experimental A-star pathfinding setup

---
+++ src/components/Experimental/ArtInt.js
@@ -0,0 +1,10 @@
+export const ArtInt = {
+  aStar: function (start, goal, grid) {
+    let path = [];
+    
+    //TEST
+    path = [{x:1,y:0},{x:1,y:1},{x:0,y:1},{x:0,y:2},{x:0,y:3},{x:0,y:4},];
+    
+    return path;
+  },
+};
---
+++ src/components/Experimental/Index.jsx
@@ -0,0 +1,63 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { ArtInt } from './ArtInt.js';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.grid = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,1,1,1,1,0,1,1,0],
+      [0,1,1,0,0,0,0,0,1,0],
+      [0,1,0,0,0,0,0,0,0,0],
+      [0,1,0,1,0,0,0,0,0,0],
+      [0,0,0,1,0,0,1,0,1,1],
+      [0,0,0,0,0,0,1,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,1,1,1,1,0,0,0,1,0],
+      [0,0,0,0,0,0,0,0,0,0],
+    ];
+    
+    this.start = { x: 1, y: 0, };
+    this.goal = { x: 9, y: 9, };
+    this.path = ArtInt.aStar(this.start, this.goal, this.grid);
+  }
+  
+  render() {
+    return (
+      <div className="experimental">
+        <table>
+        {this.grid.map((row, y) => {
+          return (
+            <tr>
+              {row.map((col, x) => {
+                let cellClass = '';
+                let cellContent = '';
+                
+                if (col > 0) cellClass += 'wall ';
+                if (this.start.x === x && this.start.y === y) cellContent += 'S';
+                if (this.goal.x === x && this.goal.y === y) cellContent += 'G';
+                
+                const isOnPath = this.path.find((step) => {
+                  return step.x === x && step.y === y;
+                });
+                if (isOnPath) cellContent += '.';
+                if (isOnPath) cellClass += 'path ';
+                
+                return (
+                  <td className={cellClass}>
+                    {cellContent}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+        </table>
+      </div>
+    );
+  }
+};
+
+export default Index;
