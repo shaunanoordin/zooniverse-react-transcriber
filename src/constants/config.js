0aef346fe3e77fb8c68db0f0afa488d0daf0b8bf
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-04-25 16:05:58 +0100

    Comment update for panoptesAppId

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,3 @@
 export const config = {
-  panoptesAppId: 'c25caf9781db436c7d7ec0cfe81926696d24886908d15cbfe0c63391a627832b',  //Panoptes-Staging ID for Zooniverse React Transcriber
+  panoptesAppId: 'c25caf9781db436c7d7ec0cfe81926696d24886908d15cbfe0c63391a627832b',  //Panoptes ID for Zooniverse React Transcriber
 };
